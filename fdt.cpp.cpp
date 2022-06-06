09c95733aa69613e771c1273e6c9f973579f358c
AryanGiri01 <102289283+AryanGiri01@users.noreply.github.com>
2022-06-06 13:41:44 +0530

    Removed cout float

--- fdt.cpp.cpp
+++ fdt.cpp.cpp
@@ -10,7 +10,6 @@ int main()
    cout << " The sizeof(int) is :           " << sizeof(int) << " bytes \n" ;
    cout << " The sizeof(long) is :          " << sizeof(long) << " bytes \n" ;
    cout << " The sizeof(long long) is :     " << sizeof(long long) << " bytes \n";
-   cout << " The sizeof(float) is :         " << sizeof(float) << " bytes \n" ;
    cout << " The sizeof(double) is :        " << sizeof(double) << " bytes \n";
    cout << " The sizeof(long double) is :   " << sizeof(long double) << " bytes \n";
    cout << " The sizeof(bool) is :          " << sizeof(bool) << " bytes \n\n";
